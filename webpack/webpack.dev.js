7db16cdecc77f372b8dca337120742ce9e1031ec
kong <peeradech_wongkum@cmu.ac.th>
2024-03-04 22:16:51 +0700

    Init

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,23 @@
+const { merge } = require("webpack-merge"); 
+const common = require("./webpack.common");
+const webpack = require("webpack");
+
+const dev = { 
+    mode: "development", 
+    stats: "errors-warnings",
+    devServer: {
+        compress: true,
+        port: 8080,
+        https: true
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "CANVAS_RENDERER": JSON.stringify(true),
+            "WEBGL_RENDERER": JSON.stringify(true),
+            "PLUGIN_FBINSTANT": JSON.stringify(true),
+            "typeof PLUGIN_FBINSTANT": JSON.stringify(true)
+    })
+    ]
+};
+
+module.exports = merge(common, dev);
